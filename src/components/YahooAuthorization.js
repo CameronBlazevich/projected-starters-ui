f5b1b9873695e5b7e1d96f68216cafdbf6d4262b
THREADRIPPER\camer <cameron.blazevich@gmail.com>
2023-04-11 16:48:05 -0500

    fixup env vars

--- src/components/YahooAuthorization.js
+++ src/components/YahooAuthorization.js
@@ -4,7 +4,8 @@ import { Button } from "reactstrap";
 function YahooAuthorization() {
     const handleClick = () => {
         const client_id = process.env.REACT_APP_YAHOO_CLIENT_ID;
-        const redirect_uri = encodeURI("https://1040-136-49-251-223.ngrok-free.app")
+        
+        const redirect_uri = encodeURI(process.env.REACT_APP_YAHOO_REDIRECT_URI)
         const url = `https://api.login.yahoo.com/oauth2/request_auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&language=en-us`
         console.log(url)
         //location.href = url;
