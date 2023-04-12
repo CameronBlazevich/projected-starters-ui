c942493b1a78a5229e434c805f0bc30b4273ef5c
THREADRIPPER\camer <cameron.blazevich@gmail.com>
2023-04-11 19:02:18 -0500

    fuck that eslint

--- src/components/YahooAuthorization.js
+++ src/components/YahooAuthorization.js
@@ -8,6 +8,7 @@ const YahooAuthorization = () => {
         const redirect_uri = encodeURI(process.env.REACT_APP_YAHOO_REDIRECT_URI)
         const url = `https://api.login.yahoo.com/oauth2/request_auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&language=en-us`
         console.log(url)
+        // eslint-disable-next-line
         location.href = url;
     }
 
