aa8c136e1a3b054f2d17ff8e1e3cafed73ca71ed
THREADRIPPER\camer <cameron.blazevich@gmail.com>
2023-04-18 14:42:52 -0500

    refactor appheader

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,18 +1,24 @@
-import {Col, Row} from 'reactstrap'
-import YahooAuthorization from './YahooAuthorization'
+import {Col, Row, Button} from 'reactstrap'
 import Logout from './auth/logout'
 
 const AppHeader = (props) => {
+    const handleClick = () => {
+        const client_id = process.env.REACT_APP_YAHOO_CLIENT_ID;
+        
+        const redirect_uri = encodeURI(process.env.REACT_APP_YAHOO_REDIRECT_URI)
+        const url = `https://api.login.yahoo.com/oauth2/request_auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&language=en-us`;
+        // eslint-disable-next-line
+        location.href = url;
+    }
     
     return (
     <Row className="App-header">
-        <Col className="logged-in-email">{props.email}</Col>
-        <Col>
+        <Col className="logged-in-email">{props.email}
             <Logout logout={props.logout}></Logout>
         </Col>
-        <Col></Col>
-        <Col>
-            <YahooAuthorization toggleModal={props.toggleModal}></YahooAuthorization>
+
+        <Col className="yahoo-auth"><span className="d-none d-sm-block">Click to Authorize</span>
+            <Button className="margin-left-5" size="sm" onClick={() => handleClick()}>Yahoo</Button>
         </Col>
     </Row>
     )
--- src/components/auth/logout.js
+++ src/components/auth/logout.js
@@ -2,7 +2,7 @@ import { Button } from "reactstrap";
 
 const Logout = (props) => {
 
-    return <div><Button size="sm" onClick={props.logout}>Logout</Button></div>
+    return <div className="margin-left-5"><Button size="sm" onClick={props.logout}>Logout</Button></div>
 }
 
 export default Logout;
